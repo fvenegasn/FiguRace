5cc471622e8dbc1e35cbe1c92b9539c8b86b7874
Julia Caro <juliac.3195@gmail.com>
2022-06-16 14:29:11 -0300

    cambios en generar_tarjeta

--- common/generar_tarjeta.py
+++ common/generar_tarjeta.py
@@ -8,12 +8,13 @@ def generar_tarjeta(dataset_actual,cant_caracteristicas):
     caracteristicas,nombre_correcta,nombre_incorrectas = opciones_random(dataset_actual,cant_caracteristicas)
 
     #----BOTONES DE OPCIONES DISPUESTOS EN FORMA ALEATORIA----#
-    correcta = [sg.Button(nombre_correcta,key="ok,"+nombre_correcta + ",intento",border_width=2,button_color='LavenderBlush3',size=(20,1))]
+    #key = estado,evento,texto_ingresado,respuesta
+    correcta = [sg.Button(nombre_correcta,key='ok,intento,'+nombre_correcta+','+nombre_correcta,border_width=2,button_color='LavenderBlush3',size=(20,1))]
 
-    opciones = [[sg.Button(x,key="error," + x + ",intento",border_width=2,button_color='LavenderBlush3',size=(20,1))] for x in nombre_incorrectas]
+    opciones = [[sg.Button(x,key='error,intento,'+x+','+nombre_correcta,border_width=2,button_color='LavenderBlush3',size=(20,1))] for x in nombre_incorrectas]
 
     opciones.insert(random.randrange(5),correcta)
 
     pistas = [[sg.Text(x,font = ('Arial,12'))] for x in caracteristicas]
 
-    return opciones,pistas,nombre_correcta
+    return opciones,pistas
