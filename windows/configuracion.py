719616fea1bd512f0479de00c9477386a55407ff
emrome <m.emiliaromero4@gmail.com>
2022-06-16 13:21:46 -0300

    Visualización parametros en configuración/Correción exit(1) spotify

--- windows/configuracion.py
+++ windows/configuracion.py
@@ -1,13 +1,24 @@
 import PySimpleGUI as sg
+from common.archivo import leer_json_data
+from common.paths import ruta_configuracion
 from common.hacer_ventana import crear_ventana
-from common.manejo_datos_juego import guardar_dato
-from common.manejo_datos_juego import mostrar_seleccionado
+from common.manejo_datos_juego import guardar_dato,mostrar_seleccionado
 from common.guardar_parametros import guardar_parametros
 from helpers.transformar_valores import values_to_options
 from common.parametros import options
 import copy
+
+
 options_copy = copy.deepcopy(options)
 
+def actualizar_parametros(window,dificultad):
+    parametros=leer_json_data(ruta_configuracion)
+    
+    window['-TIEMPO-'].update(parametros[dificultad]['tiempo_limite'])
+    window['-RONDAS-'].update(parametros[dificultad]["cant_rondas"])
+    window['-CORRECTAs-'].update(parametros[dificultad]["rta_correcta"])
+    window['-INCORRECTAs-'].update(parametros[dificultad]["rta_incorrecta"])
+    window['-CANT_CARACT-'].update(parametros[dificultad]["cant_caracteristicas"])
 
 """-------------------------INTERFAZ-------------------------------"""
 def interface():
@@ -25,21 +36,33 @@ def interface():
         sg.Button("MEDIA", key="-MEDIA-",font=('Arial',12)),
         sg.Button("DIFICIL", key="-DIFICIL-",font=('Arial',12))
     ]
-
-    layout_paremetros = [
-        [sg.Text('Ingrese el tiempo límite (en segundos)'), sg.InputText()],
-        
-        [sg.Text('Ingrese la cantidad de rondas por juego'), sg.InputText()],
-        
-        [sg.Text('Ingrese el puntaje sumado por cada respuesta correcta'), sg.InputText()],
-        
-        [sg.Text('Ingrese el puntaje sumado por cada respuesta incorrecta'), sg.InputText()],
-        
-        [sg.Text('Ingrese la cantidad de características a mostrar'), sg.InputText()],
-
+    parametros=leer_json_data(ruta_configuracion)
+    dificultad_elegida=mostrar_seleccionado('dificultad')
+    
+    layout_parametros = [
+        [
+            sg.Text('Ingrese el tiempo límite (en segundos)'), 
+            sg.InputText(parametros[dificultad_elegida]['tiempo_limite'],key='-TIEMPO-')
+        ],
+        [
+            sg.Text('Ingrese la cantidad de rondas por juego'), 
+            sg.InputText(parametros[dificultad_elegida]["cant_rondas"],key='-RONDAS-')
+        ],
+        [
+            sg.Text('Ingrese el puntaje sumado por cada respuesta correcta'), 
+            sg.InputText(parametros[dificultad_elegida]["rta_correcta"],key='-CORRECTAs-')
+        ],
+        [
+            sg.Text('Ingrese el puntaje sumado por cada respuesta incorrecta'), 
+            sg.InputText(parametros[dificultad_elegida]["rta_incorrecta"],key='-INCORRECTAs-')
+        ],
+        [
+            sg.Text('Ingrese la cantidad de características a mostrar'), 
+            sg.InputText(parametros[dificultad_elegida]["cant_caracteristicas"],key='-CANT_CARACT-')
+        ],
         [
             sg.Button("Modificar dificultad:", key="-MODIFICAR-",font=('Arial',10)),
-            sg.Text(mostrar_seleccionado('dificultad'),key='-MOSTRAR_DIFICULTAD-',font=('Arial',10))
+            sg.Text(dificultad_elegida,key='-MOSTRAR_DIFICULTAD-',font=('Arial',10))
         ]
 
     ]
@@ -48,7 +71,7 @@ def interface():
     layout = [
         [layout_dataset],
         [layout_dificultad],
-        [layout_paremetros],
+        [layout_parametros],
         [
             sg.Button('Seleccionar configuración por defecto', key="-DEFAULT-"),
             sg.Button("Volver al menú", key="-VOLVER-",font=('Arial',13))
@@ -86,7 +109,9 @@ def logistica(event,values,**kwargs):
     return True
 
 def update_windows(window,**kwargs):
-    window['-MOSTRAR_DIFICULTAD-'].update(mostrar_seleccionado('dificultad'))
+    dificultad_elegida=mostrar_seleccionado('dificultad')
+    window['-MOSTRAR_DIFICULTAD-'].update(dificultad_elegida)
+    actualizar_parametros(window,dificultad_elegida)
 
 """-------------------------EJECUCIÓN------------------------------"""
 def ejecutar():
