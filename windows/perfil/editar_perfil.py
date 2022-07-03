269198c2340341b8a5f5a52a4002efa0e68f83bf
emrome <m.emiliaromero4@gmail.com>
2022-07-03 16:20:14 -0300

    Display de los datos del usuario en 'Editar Perfil'

--- windows/perfil/editar_perfil.py
+++ windows/perfil/editar_perfil.py
@@ -1,8 +1,10 @@
+from select import select
 import PySimpleGUI as sg
 from common.hacer_ventana import crear_ventana
 from common.modificar_perfil import editar_perfil
 from common.generos import lista_generos
 from common.manejo_datos_juego import mostrar_seleccionado
+from common.manejo_datos_juego import buscar_usuario
 
 """-------------------------LOGÍSTICA------------------------------"""
 def logistica(event,values,**kwargs):
@@ -24,20 +26,18 @@ def logistica(event,values,**kwargs):
 
 """-------------------------EJECUCIÓN------------------------------"""
 def ejecutar():
+    usuario=buscar_usuario(mostrar_seleccionado('perfil'))
     layout = [[sg.VPush()],
         [
             sg.Text("Figuracer: ",font=('Arial',15)),
-            sg.Text(
-                mostrar_seleccionado('perfil'),key='-MOSTRAR_NICK-',
-                font=('Arial',15),background_color='LavenderBlush3'
-                )
+            sg.Text(usuario['nick'],key='-MOSTRAR_NICK-',font=('Arial',15),background_color='LavenderBlush3')
         ],
         
         [sg.Text('Ingrese nuevos datos:')],
         
-        [sg.Text('Edad:', size =(17, 1)), sg.InputText(size=(18,1))],
+        [sg.Text('Edad:', size =(17, 1)), sg.InputText(usuario['edad'],size=(18,1))],
         
-        [sg.Text('Género autopercibido:', size =(17, 1)), sg.Combo(values=lista_generos,size=(16,1))],
+        [sg.Text('Género autopercibido:', size =(17, 1)), sg.Combo(default_value=usuario['genero'],values=lista_generos,size=(16,1))],
         
         [sg.Button("Aceptar", key="-ACEPTAR-"), sg.Button("Cancelar", key="-CANCELAR-")],
         [sg.VPush()]
